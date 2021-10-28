80dec80d2aac56c2da199ded91387af9e33958f1
Douglas Lovera <doulovera@gmail.com>
2021-10-27 19:28:43 -0500

    Class #5 - useContext

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ThemeContext from '../context/ThemeContext'
 
 export default function Header({ isDarkMode, toggleDarkMode }) { 
+  const color = useContext(ThemeContext)
+
   return (
-    <div className="Header">
+    <div className="Header" style={{ color }}>
       <h1>ReactHooks</h1>
       <button type="button" onClick={toggleDarkMode}>{isDarkMode ? 'Dark mode' : 'Light mode'}</button>
     </div>
